5457bcddea94934a611cc326b5fb9a9ad24f04fd
Ricardo Paulino do Nascimento <ricardo.paulino.nascimento@gmail.com>
2022-02-28 15:17:02 -0300

    Copiando string com biblioteca string.h strcpy

--- main.c
+++ main.c
@@ -1,18 +1,14 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-
+#include <string.h>
 int main(int argc, char *argv[])
 {
   char palavra[50];
   
   printf("Digite uma palavra: \n");
-  scanf("%s", palavra);
+  gets(palavra);
   printf("\nPalavra digitada foi %s\n\n",palavra);
-  
-  // Mudando a primeira letra da string
-  palavra[0] = 'L';
-  printf("Palavra com nova inicial -> %s\n\n",palavra);
   
   // Copiar o conteúde de uma string para outra manualmente
   char copia_1[50];
@@ -20,8 +16,28 @@ int main(int argc, char *argv[])
   for(i = 0; i < 50; i++)
         copia_1[i] = palavra[i];      
   copia_1[i] = "\0";
+  printf("Copia #1 manual da palavra -> %s\n\n",copia_1);
   
-  printf("Palavra copiada -> %s\n\n",copia_1);
+  // Copiar o conteúdo de uma string utilizando strcpy
+  char copia_2[60];
+  
+  strcpy(copia_2, "alguma coisa digitada");
+  printf("Copia #2 manual da palavra -> %s\n\n",copia_2);
+  
+  printf("--------------- Address Memory ---------------\n\n");
+  printf("Address palavra = %d\n", &palavra);
+  printf("Address copia_1 = %d\n", &copia_1);
+  printf("Address copia_2 = %d\n", &copia_2);
+  
+   char src[40];
+   char dest[100];
+  
+   memset(dest, '\0', sizeof(dest));
+   strcpy(src, "This is tutorialspoint.com");
+   strcpy(dest, src);
+
+   printf("Final copied string : %s\n", dest);
+ 
   system("PAUSE");	
   return 0;
 }
